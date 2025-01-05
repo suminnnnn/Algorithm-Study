fdf7bbd4c7a95d2098ac8de9db3c353594cf65a0
suminnnnn <tlsssm1212@ajou.ac.kr>
2025-01-05 23:57:00 +0900

    2025-01-05 백준 1874번

---
+++ boj/1874번 스택 수열/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    
+    public static void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i=0;i<n;i++) {
+            int num = Integer.parseInt(br.readLine());
+            
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main.solution();
+    }
+}

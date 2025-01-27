6699d5d29d32e318ca2abaedfd293bbeb63a90a6
suminnnnn <tlsssm1212@ajou.ac.kr>
2025-01-28 02:03:58 +0900

    2025-01-25 백준 1463번

---
+++ boj/1463번 1로 만들기/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int D[];
+    
+    public static void solution() throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        D = new int[N+1];
+
+        D[1] = 0;
+
+        for(int i=2; i<=N; i++){
+            D[i] = D[i-1] + 1;
+            if(i%2==0) D[i] = Math.min(D[i], D[i/2] +1);
+            if(i%3==0) D[i] = Math.min(D[i], D[i/3] +1);
+        }
+
+        System.out.println(D[N]);      
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main.solution();
+    }
+}

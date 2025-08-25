fa7555085072dd7f78207fc868b53563c6f5cae5
suminnnnn <tlsssm1212@ajou.ac.kr>
2025-08-25 09:34:40 +0900

    2025-08-25 백준 11444번

--- boj/11444번 피보나치 수 6/Main.java
+++ boj/11444번 피보나치 수 6/Main.java
@@ -1,25 +1,43 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Main{
+public class Main {
 
-    static final long NUMBER = 1_000_000_007L;
-    static long[] dp;
+    static final long MOD = 1_000_000_007L;
 
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
 
-        long N = sc.nextLong();
+        long[][] result = pow(new long[][] {
+                {1, 1},
+                {1, 0}
+        }, n);
 
-        dp = new long[(int)((N+1))];
+        System.out.println(result[0][1]);
+    }
 
-        dp[0] = 0%NUMBER;
-        dp[1] = 1%NUMBER;
+    private static long[][] pow(long[][] matrix, long exp) {
+        if (exp == 1L) return matrix;
 
-        for(int i = 2; i<=N; i++){
-            dp[i] = (dp[i-1]%NUMBER) + (dp[i-2]%NUMBER);
-            dp[i] = dp[i]%NUMBER;
+        long[][] half = pow(matrix, exp / 2);
+        long[][] squared = multiply(half, half);
+
+        if (exp % 2 == 1) {
+            return multiply(squared, matrix);
+        } else {
+            return squared;
         }
-        System.out.println(dp[(int)N]);
+    }
+
+    private static long[][] multiply(long[][] a, long[][] b) {
+        long[][] result = new long[2][2];
+
+        result[0][0] = (a[0][0]*b[0][0] + a[0][1]*b[1][0]) % MOD;
+        result[0][1] = (a[0][0]*b[0][1] + a[0][1]*b[1][1]) % MOD;
+        result[1][0] = (a[1][0]*b[0][0] + a[1][1]*b[1][0]) % MOD;
+        result[1][1] = (a[1][0]*b[0][1] + a[1][1]*b[1][1]) % MOD;
+
+        return result;
     }
 }
